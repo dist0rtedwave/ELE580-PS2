ed052aa20cb66d33c708e54cdc49165aa9461451
Joey Dodds <dist0rtedwave@gmail.com>
2011-03-14 15:01:38 -0400

    a

--- ModelChecker/src/logic/model/Visitor.java
+++ ModelChecker/src/logic/model/Visitor.java
@@ -9,46 +9,60 @@ public class Visitor<G> {
 	  this.g = g;
 	}
 	
-	public void visit(Expression o){
+	public void visit(Expression o, Boolean PrintExpr){
 		switch (o.getDescriptor()){
 		case Variable.DESCRIPTOR:
-			visitVariable((Variable)o);
+			visitVariable((Variable)o, PrintExpr);
 			break;
 		case BinaryOp.DESCRIPTOR:
-			visitBinaryOp((BinaryOp)o);
+			visitBinaryOp((BinaryOp)o, PrintExpr);
 			break;
 		case UnaryOp.DESCRIPTOR:
-			visitUnaryOp((UnaryOp)o);
+			visitUnaryOp((UnaryOp)o, PrintExpr);
 			break;
 		}
 	}
 
-	protected void visitExpression(Expression o){
+	protected void visitExpression(Expression o, Boolean PrintExpr){
 		assert o!=null;
 	}
 	
-	protected void visitVariable(Variable o) {
+	protected void visitVariable(Variable o, Boolean PrintExpr) {
 		assert o != null;
-		visitExpression(o);
+		if(PrintExpr)
+			System.out.print(o.getTheName());
+		visitExpression(o, PrintExpr);
 	}
 	
-	protected void visitBinaryOp(BinaryOp o) {
+	protected void visitBinaryOp(BinaryOp o, Boolean PrintExpr) {
 		assert o != null;
+		if(PrintExpr)
+			System.out.print(" (");
 		if(o.theLHS!=null){
-			visit(o.theLHS);
+			visit(o.theLHS, PrintExpr);
+		}
+		if(PrintExpr)
+		{
+			System.out.print(") ");
+			System.out.print(o.getTheBinaryOperator());
+			System.out.print(" (");
 		}
 		if(o.getTheRHS()!=null){
-			visit(o.theRHS);
+			visit(o.theRHS, PrintExpr);
 		}
-		visitExpression(o);
+		if(PrintExpr)
+			System.out.print(") ");
+		visitExpression(o, PrintExpr);
 	}
 	
-	protected void visitUnaryOp(UnaryOp o){
+	protected void visitUnaryOp(UnaryOp o, Boolean PrintExpr){
 		assert o!=null;
+		if(PrintExpr)
+			System.out.print(o.getTheOperator());
 		if(o.theExpression!=null){
-			visit(o.theExpression);
+			visit(o.theExpression, PrintExpr);
 		}
-		visitExpression(o);
+		visitExpression(o, PrintExpr);
 	}
 
 }
