b86bd6d1290b9ddb9179b63a05b532a3b0993e3c
Joey Dodds <dist0rtedwave@gmail.com>
2011-03-15 14:45:37 -0400

    First version of converter

--- ModelChecker/src/logic/converter/Converter.java
+++ ModelChecker/src/logic/converter/Converter.java
@@ -4,31 +4,72 @@ package logic.converter;
 
 import logic.model.*;
 
-public class Converter extends Visitor<Expression> {
+public class Converter extends Visitor<ConverterContext> {
 
-	Boolean OrSeen = false;		//keeps track of whether or not an OR has been seen. If it has, then we cannot see ANDs
-	public Converter(Expression g) {
+	public Converter(ConverterContext g) {
 		super(g);
 	}
 	
-	protected void visitVariable(Variable o) {
-		
-	}
-	
+	//assumes only and and or
 	protected void visitBinaryOp(BinaryOp o) {
-		if(o.getTheBinaryOperator().compareTo(BinaryOperator.AND)==0)
-		{
-			if(OrSeen)
+		if(!this.g.isMakingEquivalences()){
+			if(o.getTheBinaryOperator().compareTo(BinaryOperator.AND)==0)
 			{
-				//need to emit a fresh variable and append an equivalence to the end of total expression
+				if(this.g.isOrSeen())
+				{
+					this.g.setMakingEquivalences(true);
+				}
 			}
+			else{
+				this.g.setOrSeen(true);
+			}
+		}
+		visit(o.getTheLHS());
+		if(this.g.isMakingEquivalences()){
+			Variable v = new Variable();
+			v.setTheName("_t" + this.g.getFreshNumber());
+			this.g.setFreshNumber(this.g.getFreshNumber()+1);
+			o.setTheLHS(v);
+			
+			BinaryOp equiv = new BinaryOp();
+			equiv.setTheBinaryOperator(BinaryOperator.EQUIV);
+			Variable newT = new Variable();
+			newT.setTheName("_t" + this.g.getFreshNumber());
+			equiv.setTheLHS(newT);
+			equiv.setTheRHS(o);
+			
+			BinaryOp newExpr = new BinaryOp();
+			newExpr.setTheLHS(g.getTheExpression());
+			newExpr.setTheRHS(equiv);
+			newExpr.setTheBinaryOperator(BinaryOperator.AND);
+			
+			g.setTheExpression(newExpr);
+		}
+		visit(o.getTheRHS());
+		if(this.g.isMakingEquivalences()){
+			Variable v = new Variable();
+			v.setTheName("_t" + this.g.getFreshNumber());
+			this.g.setFreshNumber(this.g.getFreshNumber()+1);
+			o.setTheRHS(v);
+			
+			BinaryOp equiv = new BinaryOp();
+			equiv.setTheBinaryOperator(BinaryOperator.EQUIV);
+			Variable newT = new Variable();
+			newT.setTheName("_t" + this.g.getFreshNumber());
+			equiv.setTheLHS(newT);
+			equiv.setTheRHS(o);
+			
+			BinaryOp newExpr = new BinaryOp();
+			newExpr.setTheLHS(g.getTheExpression());
+			newExpr.setTheRHS(equiv);
+			newExpr.setTheBinaryOperator(BinaryOperator.AND);
+			
+			g.setTheExpression(newExpr);
+	
 		}
 		
 	}
 	
-	protected void visitUnaryOp(UnaryOp o){
-		
-	}
 
 
 }
